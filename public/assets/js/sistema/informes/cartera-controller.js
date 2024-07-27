044ad1ccc269e467048c71f1dc757b51a0b120eb
Abdel cartagena castro <abdel_123@hotmail.es>
2024-07-27 13:35:22 -0500

    Actualizar informe de cartera

--- public/assets/js/sistema/informes/cartera-controller.js
+++ public/assets/js/sistema/informes/cartera-controller.js
@@ -46,7 +46,7 @@ function carteraInit() {
                     $('td', row).css('color', 'white');
                     return;
                 }
-                if (parseInt(data.saldo) < 0 || parseInt(data.saldo_anterior) < 0 ) $('td', row).css('background-color', 'rgb(209 64 64 / 40%)');
+                if (data.errores) $('td', row).css('background-color', 'rgb(209 64 64 / 40%)');
             } else if (nivel == 2) {
                 if (data.nivel == 0) {
                     $('td', row).css('background-color', 'rgb(28 69 135)');
@@ -55,7 +55,7 @@ function carteraInit() {
                     return;
                 }
                 if(data.nivel == 1){
-                    if (parseInt(data.saldo) < 0 || parseInt(data.saldo_anterior) < 0 ) $('td', row).css('background-color', 'rgb(209 64 64 / 40%)');
+                    if (data.errores) $('td', row).css('background-color', 'rgb(209 64 64 / 40%)');
                     else $('td', row).css('background-color', 'rgb(64 164 209 / 40%)');
                     $('td', row).css('font-weight', 'bold');
                     return;
@@ -68,8 +68,8 @@ function carteraInit() {
                     return;
                 }
                 if(data.nivel == 1){
-                    if (parseInt(data.saldo) < 0 || parseInt(data.saldo_anterior) < 0 ) $('td', row).css('background-color', 'rgb(209 64 64 / 40%)');
-                    else $('td', row).css('background-color', 'rgb(64 164 209 / 40%)');
+                    if (data.errores) $('td', row).css('background-color', 'rgb(209 64 64 / 40%)');
+                    else $('td', row).css('background-color', 'rgb(64 164 209 / 70%)');
                     $('td', row).css('font-weight', 'bold');
                     return;
                 }
