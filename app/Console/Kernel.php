52f870c4416e0174f90394d740f96c81b3e2bd4b
Abdel cartagena castro <abdel_123@hotmail.es>
2025-05-29 16:26:03 -0500

    Agregar cron cada 24h

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:validar-pagos-pasarela')->cron('*/12 * * * *');
+        $schedule->command('app:validar-pagos-pasarela')->dailyAt('03:00');
     }
 
     /**
