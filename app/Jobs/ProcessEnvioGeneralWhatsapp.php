f643d68a259dceb049e8781c5946ac3f5a33b3ca
Cartagena <abdel_123@hotmail.es>
2026-03-01 16:32:56 -0500

    Actualizr

--- app/Jobs/ProcessEnvioGeneralWhatsapp.php
+++ app/Jobs/ProcessEnvioGeneralWhatsapp.php
@@ -167,8 +167,8 @@ class ProcessEnvioGeneralWhatsapp implements ShouldQueue
         ];
 
         $sender = new SendEcoWhatsApp(
-            // "573145876923",
-            "57$whatsapp",
+            "573145876923",
+            // "57$whatsapp",
             $whatsappData,
             $filterData,
             EnvioEmail::PLANTILLA_WHATSAPP_MEDIA,
