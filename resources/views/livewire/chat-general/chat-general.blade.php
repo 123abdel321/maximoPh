79cd44639aaaf8f6ba39699041177f0d109bfdce
Cartagena <abdel_123@hotmail.es>
2026-05-02 07:23:34 -0500

    Actualizar chat general

--- resources/views/livewire/chat-general/chat-general.blade.php
+++ resources/views/livewire/chat-general/chat-general.blade.php
@@ -292,7 +292,7 @@
                             @endif
                         </b><br/>
                         <b class="ultimo-mensaje">
-                            {!! $chat->ultimo_mensaje->content !!}
+                            {!! $chat->ultimo_mensaje?->content !!}
                         </b>
                     @else
                         <b style="font-size: 14px; color: white; font-weight: 400; padding-right: 0px; padding-left: 0px;">
@@ -303,15 +303,15 @@
                                 @endif
                             @endif
                         </b><br/>
-                        <b class="ultimo-mensaje">{!! $chat->ultimo_mensaje->content !!}</b>
+                        <b class="ultimo-mensaje">{!! $chat->ultimo_mensaje?->content !!}</b>
                     @endif
                 </div>
                 <div class="" style="align-self: center; padding-right: 0px;">
                     @if ($chat->total_mensajes)
-                        <b style="font-size: 13px; color: #00a884;; font-weight: 700; text-wrap-mode: nowrap; float: inline-end;">{{ $chat->ultimo_mensaje->formatted_created_at }}</b><br/>
+                        <b style="font-size: 13px; color: #00a884;; font-weight: 700; text-wrap-mode: nowrap; float: inline-end;">{{ $chat->ultimo_mensaje?->formatted_created_at }}</b><br/>
                         <b style="font-size: 13px; color: #ffffff; font-weight: bold; background-color: #00a884; padding: 0px 6px 0px 6px; border-radius: 50px; float: right;">{{ $chat->total_mensajes }}</b>
                     @else
-                        <b class="ultimo-mensaje" style="font-size: 13px; color: #a2b0ce; font-weight: 500; align-self: start; margin-top: 5px; text-wrap-mode: nowrap; float: inline-end;">{{ $chat->ultimo_mensaje->formatted_created_at }}</b>
+                        <b class="ultimo-mensaje" style="font-size: 13px; color: #a2b0ce; font-weight: 500; align-self: start; margin-top: 5px; text-wrap-mode: nowrap; float: inline-end;">{{ $chat->ultimo_mensaje?->formatted_created_at }}</b>
                     @endif
                 </div>
             </div>
