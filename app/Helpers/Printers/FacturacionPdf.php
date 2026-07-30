74e70b9992696a7dadca76abe202b295a259b1c6
abdelcartagena <abdel_123@hotmail.es>
2026-07-30 12:13:08 -0500

    Actualizar concepto

--- app/Helpers/Printers/FacturacionPdf.php
+++ app/Helpers/Printers/FacturacionPdf.php
@@ -166,7 +166,7 @@ class FacturacionPdf extends AbstractPrinterPdf
 				'codigo_comprobante',
 				'nombre_comprobante',
 				'consecutivo',
-				'concepto',
+				DB::raw('MIN(concepto) AS concepto'),
 				'fecha_manual',
 				'created_at',
 				'fecha_creacion',
